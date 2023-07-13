c638dbf584fc2866301e791ba4e70c9916d7363a
RedTea <myadiuio@gmail.com>
2023-07-13 14:49:54 +0900

    Update

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require("express")
 const http = require("http")
+const got = require('got')
 const euckr = require("./module/euckrEncode.js")
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+async function getSchoolSearchKey() {
+    let result = await got("http://comci.kr:4082/st")
+    let str = result.body.toString().match(/(function school_ra\(sc\)\{\$\.ajax\(\{ url:'\.\/)[0-9]+\?[0-9]+[a-z]\'\+sc\,/).toString()
+    return (str.replace("function school_ra(sc){$.ajax({ url:'./", "").replace("'+sc,,function school_ra(sc){$.ajax({ url:'./", ""))
+}
+
+app.get("/", async (req, res) => {
     res.send("Hello World!");
 });
 
-app.get("/search", (req, res) => {
+app.get("/search", async (req, res) => {
     res.contentType("json");
     const school_name = req.query.school?.toString() ?? "";
     new TextEncoder();
-    const request_path = "/36179?17384l" + euckr.euckrEncode(school_name)
-    console.log(request_path);
+    const request_path = "/" + await getSchoolSearchKey() + euckr.euckrEncode(school_name)
+    // console.log(request_path);
     http.get(
         {
             host: "comci.kr",
